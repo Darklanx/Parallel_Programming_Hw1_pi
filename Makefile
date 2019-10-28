42b7c4c8116b8b1b8a03606269a2d4d588481c95
darklanx <csdarklanx@gmail.com>
2019-10-28 10:28:04 +0800

    graph.py
    ignore

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 TARGET = pi
-
 all: $(TARGET)
 $(TARGET): pi.cpp
 	g++ -pthread -std=c++11 -O2 -s pi.cpp -o pi
+
+.PHONY: clean
+clean:
+	rm `ls | grep "result_"`
---
+++ graph.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import sys
+
+
+def load_file(path):
+
+    return f
+
+
+def _main():
+    f = load_file(sys.argv[1])
+
+
+_main()
